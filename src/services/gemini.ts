306236125decd74e4ed2f3b637bf4c49f64e8763
Renan Borges <renanvcb@gmail.com>
2025-07-11 00:43:39 -0300

    feat: enhance createQuestion route to generate answers based on context and update question prompt

--- src/services/gemini.ts
+++ src/services/gemini.ts
@@ -8,11 +8,15 @@ const gemini = new GoogleGenAI({
 const model = 'gemini-2.5-flash'
 
 export async function transcribeAudio(audioAsBase64: string, mimeType: string) {
+  const transcribePrompt = `Transcreva o áudio para português do Brasil. Seja 
+    preciso e natural na transcrição. Mantenha a pontuação adequada e divida o 
+    texto em parágrafos quando for apropriado`.trim()
+
   const response = await gemini.models.generateContent({
     model,
     contents: [
       {
-        text: 'Transcreva o áudio para português do Brasil. Seja preciso e natural na transcrição. Mantenha a pontuação adeqiada e divida o texto em parágrafos quando for apropriado',
+        text: transcribePrompt,
       },
       {
         inlineData: {
@@ -48,4 +52,45 @@ export async function generateEmbeddings(text: string) {
   }
 
   return response.embeddings[0].values
+}
+
+export async function generateAnswer(
+  question: string,
+  transcriptions: string[]
+) {
+  const context = transcriptions.join('\n\n')
+
+  const answerPrompt = `Com base no texto fornecido abaixo como contexto, responda a pergunta de forma clara e precisa. Em português do Brasil
+  
+  
+  CONTEXTO:
+  ${context}
+
+  PERGUNTA:
+  ${question}
+
+  INSTRUÇÕES:
+  - Use apenas informações do contexto para responder;
+  - Se a resposta não for encontrada no contexto, responda apenas que não possui informações suficientes para responder;
+  - Seja objetivo e claro na resposta;
+  - Evite repetir a pergunta na resposta;
+  - Mantenha um tom educativo e profissional;
+  - Cite trechos relevantes do contexto, se necessário, para embasar a resposta;
+  - Se for citar o contexto, utilize o termo "conteúdo da aula";
+  `
+
+  const response = await gemini.models.generateContent({
+    model,
+    contents: [
+      {
+        text: answerPrompt,
+      },
+    ],
+  })
+
+  if (!response.text) {
+    throw new Error('Failed to generate answer')
+  }
+
+  return response.text
 }
